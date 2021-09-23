52314ca9f0fd77bcae2fc245df5ba5dd9bef3a53
gustavo <gustavoborges98@hotmail.com>
2021-09-23 00:04:33 -0300

    integracao de dados calendario

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import store from '../../store/store';
+import * as S from './styles';
 
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
@@ -12,34 +13,56 @@ const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
 const PagesDashboard = () => {
   const dispatch = useDispatch();
-
   const _appointments = useSelector((state) => state.appointments);
-
   const [appointments, setAppointments] = useState(_appointments);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     async function _getAppointments() {
-      const data = await getAppointments();
+      const data = await(getAppointments());
+      console.log(data);
       await setAppointments(data);
     }
     _getAppointments();
   }, []);
 
   useEffect(() => {
-    console.log(`appointments `+ appointments)
+    console.log(`appointments ` + appointments)
     dispatch({ type: 'SET_APPOINTMENTS', payload: appointments });
   }, [appointments]);
 
+  useEffect(() => {
+    const array = [];
+    Object.values(appointments).map((appointment) => {
+      console.log(appointment)
+      array.push({
+        title: appointment.name,
+        start: appointment.initialDate,
+        end: appointment.finalDate,
+        allDay: false, 
+        resource: appointment.id
+      });
+    })
+    setEvents(array);
+  }, [appointments]);
+
+
+
   return (
-    <div>
+    <S.Dashboard>
+      <S.DashboardLeft className="col-lg-4 col-sm-12">
+        <h1>ola</h1>
+      </S.DashboardLeft>
+      <S.DashboardRight className="col-lg-8 col-sm-12" >
       <Calendar
-        localizer={localizer}
-        events={appointments}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-    </div>
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </S.DashboardRight>
+    </S.Dashboard>
   );
 }
 
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Dashboard = styled.div`
+  display: flex;
+  padding: 1rem 2rem;
+`
+
+export const DashboardLeft = styled.div`
+  width: 40%;
+`
+
+export const DashboardRight = styled.div`
+  width: 60%;
+`
